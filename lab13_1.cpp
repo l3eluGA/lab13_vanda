dd0cf436f676c53950522d5a28b86806476837b4
l3eluGA <rungthiporange@gmail.com>
2023-01-10 15:02:41 +0700

    lab13_1completed

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -1,6 +1,17 @@
 #include<iostream>
 using namespace std;
 
+int fibonacci(unsigned int a)
+{
+    if(a==0 or a==1)
+    {
+        return a;
+    }
+    else if(x>1)
+    {
+        return fibonacci(x-1)+fibonacci(x-2);
+    } 
+}
 int main(){
     int x;
     cin >> x;
